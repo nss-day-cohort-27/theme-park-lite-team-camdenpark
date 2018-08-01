37bcb1c61dd3d51b625d12e9a0871c433359e436
Grady Robbins <gradyrobbins13@gmail.com>
2018-08-01 14:07:45 -0500

    add typesOfAttractions.js and linked to main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@
 // This is here to get you started. You may remove or move as necessary.
 let parkInfo = require('./parkInfo.js');//mark
 let attractions = require(`./attractions.js`);//gretchen
-let attractions = require(`./attractions.js`);
+let typesOfAttraction = require(`./typesOfAttractions.js`);  // grady's js
 let attractions = require(`./attractions.js`);
 let attractions = require(`./attractions.js`);
