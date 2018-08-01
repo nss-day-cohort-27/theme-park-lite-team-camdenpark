eec972babeb8bccc68c8614eb6b5315a098ab767
Grady Robbins <gradyrobbins13@gmail.com>
2018-08-01 15:08:17 -0500

    add function to typesOfAttractions.js

--- js/typesOfAttractions.js
+++ js/typesOfAttractions.js
@@ -4,5 +4,17 @@
 console.log("Hello world typesOfAttractions.js");
 
 
+let db = require("./db-calls.js");
+
+let typesOfAttractions = {};
+
+db.fetchtypesOfAttractions()
+   .then((result) => {
+   typesOfAttractions = result;
+});
+
+module.exports = typesOfAttractions;
+
+
 
 
