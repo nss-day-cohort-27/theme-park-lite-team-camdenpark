324bb9a4360ae493dd45a6603e0fac2ca52f96cd
Mark Hale <mark.hale94@gmail.com>
2018-08-01 10:59:25 -0500

    added stuff

--- js/db-calls.js
+++ js/db-calls.js
@@ -7,10 +7,24 @@ let dbCalls = {};
 let parkInfo = {};
 let attractions = {};
 let attractionTypes = {};
+let areaTypes = {};
+
+dbCalls.fetchAreas = () => {
+  return fetch("https://external-data-56670.firebaseio.com/areas.json")
+    .then(res => res.json())
+    .then((result) => {
+      // console.log(typeof result);
+      areaTypes = result;
+      return areaTypes;
+    },
+    (error) => {
+       return error;
+    });
+};
 
 
 dbCalls.fetchParkInfo = () => {
-    return fetch(`https://external-data-56670.firebaseio.com/park-info.json`)
+    return fetch("https://external-data-56670.firebaseio.com/park-info.json")
       .then(res => res.json())
       .then((result) => {
         // console.log(typeof result);
@@ -24,7 +38,7 @@ dbCalls.fetchParkInfo = () => {
 };
 
 dbCalls.fetchAttractions = () => {
-  return fetch(`https://external-data-56670.firebaseio.com/attractions.json`)
+  return fetch("https://external-data-56670.firebaseio.com/attractions.json")
     .then(res => res.json())
     .then((result) => {
       // console.log(typeof result);
@@ -37,7 +51,7 @@ dbCalls.fetchAttractions = () => {
 };
 
 dbCalls.fetchTypes = () => {
-  return fetch(`https://external-data-56670.firebaseio.com/attraction_types.json`)
+  return fetch("https://external-data-56670.firebaseio.com/attraction_types.json")
     .then(res => res.json())
     .then((result) => {
       // console.log(typeof result);
@@ -50,7 +64,7 @@ dbCalls.fetchTypes = () => {
 };
 
 dbCalls.getParkInfo = () => {
-  return parkInfo;  
+  return parkInfo;
 };
 
 
