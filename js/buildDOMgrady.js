7427d4a9a46fa94190d1913a4273954bbb22896d
Grady Robbins <gradyrobbins13@gmail.com>
2018-08-02 11:00:14 -0500

    format to Capital letter- typesofAttractions.js

---
+++ js/buildDOMgrady.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log("hello world buildDOMgrady.js")
+
+// build a template with backticks and a fragment to write/append to the DOM
+// require typesofAttractions.js; invoke it;
--- js/typesOfAttractions.js
+++ js/typesOfAttractions.js
@@ -1,20 +1,56 @@
 "use strict";
 
 
-console.log("Hello world typesOfAttractions.js");
+// console.log("Hello world typesOfAttractions.js");
 
 
 let db = require("./db-calls.js");
 
 let typesOfAttractions = {};
 
+let capitalizeFirstLetter = (taco) => {
+    taco.forEach((item, index) => {
+        let newResult = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+        taco[index].name = newResult;
+        console.log ("newResult" , newResult);
+      
+    })
+    return taco
+};
+
+
 db.fetchTypes()
    .then((result) => {
-   typesOfAttractions = result;
+       
+       let newResult = capitalizeFirstLetter(result)
+       typesOfAttractions = newResult;
+
+   console.log("newResult" , newResult);
+
+   
+
+//    build a function in here that capitalizes the first letter in each word
 });
 
 module.exports = typesOfAttractions;
 
 
 
+// let outputEl = document.querySelector("#attractionType");
+
+
+// function listTypes(typeData){
+//     typeData.forEach(type => {
+//         let typeComponent = createtypes(type.name.charAt(0).toUpperCase()+type.name.slice(1));
+//         writeContactsToDOM(typeComponent);
+//     });
+// }
+
+// //this is actually writing it to the DOM. and keeps adding a new component each time it goes through the for-loop up top.  
+// function writeContactsToDOM(attractionType){
+//     outputEl.innerHTML += attractionType;
+// }
+
+
+// module.exports = listTypes;
 
